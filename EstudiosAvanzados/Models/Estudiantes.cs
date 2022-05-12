92b7d374f724896e01cf4ff9cbc859812ec08bb8
ramonfor-3 <ramonfor3@gmail.com>
2022-05-12 18:19:41 -0400

    Agregar archivos de proyecto.

---
+++ EstudiosAvanzados/Models/Estudiantes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EstudiosAvanzados.Models
+{
+    public partial class Estudiantes
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre no puede estar vacío.")]
+        [StringLength(255, ErrorMessage = "Nombre no puede exceder 255 caracteres.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Fecha de Nacimiento  no puede estar vacío.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "Curso no puede estar vacío.")]
+        [StringLength(255)]
+        public string Curso { get; set; }
+
+        public string Estado { get; set; }
+
+        public int edad;
+    }
+}
---
+++ EstudiosAvanzados/Models/RegistroEstudiantesContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EstudiosAvanzados.Models
+{
+    public partial class RegistroEstudiantesContext : DbContext
+    {
+        public RegistroEstudiantesContext()
+        {
+        }
+
+        public RegistroEstudiantesContext(DbContextOptions<RegistroEstudiantesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Estudiantes> Estudiantes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("server=localhost;database=RegistroEstudiantes; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Estudiantes>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Curso)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Estado)
+                    .IsRequired()
+                    .HasMaxLength(15)
+                    .IsFixedLength();
+
+                entity.Property(e => e.FechaNacimiento).HasColumnType("date");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
